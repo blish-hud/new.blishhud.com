1fb846a929a2866ef99498fa800ad8b9218bbe01
Xander Lamkins <dade@lamkins.org>
2026-01-19 23:11:18 -0500

    Only show the tooltips when the stats have been visually reduced.

--- src/pages/module/moduleCard.jsx
+++ src/pages/module/moduleCard.jsx
@@ -99,10 +99,10 @@ export default function ModuleCard({ module }) {
 
             <div className="level-right">
               <div className="mc-stats">
-                <span className="hint--left" data-hint={`${module.Downloads.toLocaleString()} Total Downloads`}>
+                <span>
                   <svg class="mui-icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M19 9h-4V3H9v6H5l7 7zM5 18v2h14v-2z"></path></svg>
 
-                  <span className="is-hidden-mobile">
+                  <span className="is-hidden-mobile hint--left" data-hint={`${module.Downloads.toLocaleString()} Total Downloads`}>
                     &nbsp;{formatDownloads(module.Downloads)}
                   </span>
                   <span className="is-hidden-tablet">
@@ -110,10 +110,10 @@ export default function ModuleCard({ module }) {
                   </span>
                 </span>
 
-                <span className="hint--left" data-hint={`Last Updated: ${new Date(module.LastUpdate).toLocaleDateString()}`}>
+                <span>
                     <svg class="mui-icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2M12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8"></path><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"></path></svg>
                     
-                    <span className="is-hidden-mobile">
+                    <span className="is-hidden-mobile hint--left" data-hint={`Last Updated: ${new Date(module.LastUpdate).toLocaleDateString()}`}>
                       &nbsp;{timeAgo(module.LastUpdate)}
                     </span>
                     <span className="is-hidden-tablet">
